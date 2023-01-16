1ba9d542740a3e68ba2aee514de941398bddad0f
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2023-01-17 00:19:35 +0200

    ref: extract server creation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import http from "http";
-import { routeRequest } from "./routes";
-
+import { startServer } from './server';
 
 const PORT = Number(process.env.PORT) || 8080;
 
-const server = http.createServer();
-
-server.on("request", routeRequest);
-server.on("error", err => {throw err;});
-
-server.listen(PORT);
+startServer(PORT);
